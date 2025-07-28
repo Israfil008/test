42e512ae606c0dae97b4a9f20629d02581364461
Israfil008 <israfilmiya619@gmail.com>
2025-07-28 09:36:54 +0545

    a1

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,13 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvMOB7Sj537752wGyApFh2865YnTt9qHY",
-  authDomain: "pustaklink-ab06d.firebaseapp.com",
-  projectId: "pustaklink-ab06d",
-  storageBucket: "pustaklink-ab06d.firebasestorage.app",
-  messagingSenderId: "435791021437",
-  appId: "1:435791021437:web:eb6d432b04c24fbb743814",
+  apiKey: "AIzaSyDM_FfjzjNxLgsxIWKONe5JF4wdoT-M-ro",
+  authDomain: "pustaklink008.firebaseapp.com",
+  projectId: "pustaklink008",
+  storageBucket: "pustaklink008.firebasestorage.app",
+  messagingSenderId: "647353066549",
+  appId: "1:647353066549:web:6f54129c90ebc4a54afd7c",
+  measurementId: "G-YSG4XW1KT1"
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
